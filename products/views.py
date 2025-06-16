cea8c7d479a7440501e1e7eed4a494be756ca4fe
inoueUJ <yuji.inoue11.gmail.com>
2025-06-16 12:46:49 +0900

    setup

--- products/views.py
+++ products/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Product
 from .serializers import ProductSerializer
+from .filters import ProductFilter
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'price']  # 完全一致で絞り込み可能
+    filterset_class = ProductFilter
